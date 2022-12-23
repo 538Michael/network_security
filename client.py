b55d41a30e208fa182d8b330f8d104736a50a93d
LucasSantosFreire <38673594+LucasSantosFreire@users.noreply.github.com>
2022-12-23 09:59:13 -0300

    Update client.py
    changing some client config

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ from socket import *
 import ssl
 
 client = socket(AF_INET, SOCK_STREAM)
-client_ssl = ssl.wrap_socket(client, ca_certs="./cert.pem")
+client_ssl = ssl.wrap_socket(client,ciphers="AES256-GCM-SHA384", ca_certs="./cert.crt")
 
 client_ssl.connect(('127.0.0.1', 4444))
 
@@ -12,4 +12,4 @@ while True:
     if message:
         client_ssl.write(message.encode())
 
-client.close()
+#client.close()
